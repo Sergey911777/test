024e760bde732d970527e60844da96c07403046a
Sergey <stuzhikov@gmail.com>
2022-03-08 18:52:30 +0300

    New format time

--- main.go
+++ main.go
@@ -9,5 +9,5 @@ func main() {
 	gde := " В Санкт-Петербурге"
 
 	vremayNow := time.Now()
-	fmt.Println(vremayNow.Format("02-01-2006 15:04:05"), gde)
+	fmt.Println(vremayNow.Format("02/01/2006 15:04:05"), gde)
 }
